00a9a83cf419a141f5dc18c749ad7225c0abe796
didusenko <didusenko.dmytro@gmail.com>
2020-01-30 00:38:19 +0200

    2020_01_30 - Data Annotations were added to DB

--- IT_Dnistro/DataBase/Tour.cs
+++ IT_Dnistro/DataBase/Tour.cs
@@ -7,10 +7,18 @@ namespace DataBase
     {
         [Key]
         public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
         public string TourName { get; set; }
+
         public int TourTypeId { get; set; }
         public virtual TourType TourType { get; set; }
+
+        [Required]
         public DateTime TourDate { get; set; }
+
+        [Required]
         public int TourLength { get; set; }
     }
 }
--- IT_Dnistro/DataBase/TourType.cs
+++ IT_Dnistro/DataBase/TourType.cs
@@ -7,7 +7,11 @@ namespace DataBase
     {
         [Key]
         public int Id { get; set; }
+
+        [MaxLength(150)]
+        [Required]
         public string TourTypeName { get; set; }
+
         public string TourTypeDescription { get; set; }
     }
 }
