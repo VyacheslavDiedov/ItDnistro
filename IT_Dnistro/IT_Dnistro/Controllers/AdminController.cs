266381c6a3779bbbd4cb03994692ccd9e81a1a65
Vladislav <2001vlad2908@gmail.com>
2020-01-30 02:14:32 +0200

    add AdminController with Hello Admin

---
+++ IT_Dnistro/IT_Dnistro/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IT_Dnistro.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IT_Dnistro.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
