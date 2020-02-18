c1b5bdb95365c5fdf8aa621ce93f893a9744edb2
didusenko <didusenko.dmytro@gmail.com>
2020-02-18 20:35:35 +0200

    2020_02_18 database rebuild

--- IT_Dnistro/DataBase/DatabaseContext.cs
+++ IT_Dnistro/DataBase/DatabaseContext.cs
@@ -14,26 +14,11 @@ namespace DataBase
             Database.EnsureCreated();
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    // Customize the ASP.NET Identity model and override the defaults if needed.
-        //    // For example, you can rename the ASP.NET Identity table names and more.
-        //    // Add your customizations after calling base.OnModelCreating(builder);
-
-        //    modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
-        //    {
-        //        Id = Guid.NewGuid().ToString(),
-        //        Name = "Admin", NormalizedName = "Admin".ToUpper(),
-        //        ConcurrencyStamp = "rrr"
-        //    });
-        //}
-
         public DbSet<User> Users { get; set; }
-        //public DbSet<UserAdditionalInfo> UserAdditionalInfos { get; set; }
         public DbSet<Tour> Tours { get; set; }
         public DbSet<TourType> TourTypes { get; set; }
         public DbSet<UserTour> UserTours { get; set; }
+        public DbSet<Admin> Admins { get; set; }
     }
 
 }
--- IT_Dnistro/IT_Dnistro/Program.cs
+++ IT_Dnistro/IT_Dnistro/Program.cs
@@ -12,30 +12,32 @@ using DataBase;
 
 namespace IT_Dnistro
 {
-    public class Program
+    public static class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-             
-            var host = CreateHostBuilder(args).Build();
- 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    await RoleInitializer.InitializeAsync(userManager, rolesManager).ConfigureAwait(true);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
- 
-            host.Run();
+
+            //var host = CreateHostBuilder(args).Build();
+
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    try
+            //    {
+            //        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+            //        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            //        await RoleInitializer.InitializeAsync(userManager, rolesManager).ConfigureAwait(true);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred while seeding the database.");
+            //    }
+            //}
+
+            //host.Run();
+
+            CreateHostBuilder(args).Build().Run();
         }
  
 
