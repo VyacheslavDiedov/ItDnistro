d399259edc17cbcc8a59bbfaee69e6a93be150d8
Dmitriy <taniagirafee@ukr.net>
2020-03-10 21:15:52 +0200

    added DashboardController

--- IT_Dnistro/IT_Dnistro/Controllers/DashboardController.cs
+++ IT_Dnistro/IT_Dnistro/Controllers/DashboardController.cs
@@ -1,62 +1,83 @@
-﻿//using System.Linq;
-//using System.Threading.Tasks;
-//using DataBase;
-//using Microsoft.AspNetCore.Mvc;
-//using IT_Dnistro.ViewModels;
-//using Microsoft.AspNetCore.Identity;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DataBase;
+using Microsoft.AspNetCore.Mvc;
+using IT_Dnistro.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-//namespace IT_Dnistro.Controllers
-//{
-//    public class DashboardController : Controller
-//    {
-//        private readonly DatabaseContext _context;
+namespace IT_Dnistro.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly DatabaseContext _context;
 
-//        public DashboardController(DatabaseContext context)
-//        {
-//            _context = context;
-//        }
+        public DashboardController(DatabaseContext context)
+        {
+            _context = context;
+        }
 
-//        /// <summary>
-//        /// повертає список учасників для таблиці
-//        /// </summary>
-//        /// <returns></returns>
-//        [HttpGet]
-//        public IActionResult GetPartiсipants()
-//        {
-//            var items = _context.UserTours.Select(x => new ParticipantsViewModel()
-//            {
-//                UserId = x.User.Id,
-//                TourId = x.TourId,
-//                FullName = x.User.UserName,
-//                PhoneNumber = x.User.PhoneNumber,
-//                Email = x.User.Email,
-//                TourName = x.Tour.TourName
-//            }).ToList();
+        /// <summary>
+        /// повертає список учасників для таблиці
+        /// </summary>
 
-//            return View(items);
-//        }
+        [HttpGet]
+        [Route("gg")]
+        public IActionResult GetPartiсipants()
+        {
+            var items = _context.UserTours.Select(x => new ParticipantsViewModel()
+            {
+                UserId = x.User.Id,
+                TourId = x.TourId,
+                FullName = x.User.UserName,
+                PhoneNumber = x.User.PhoneNumber,
+                Email = x.User.Email,
+                TourName = x.Tour.TourName
+            }).ToList();
 
-//        /// <summary>
-//        /// додає нового учасника в поїздку
-//        /// </summary>
-//        /// <returns></returns>
-//        [HttpPost]
-//        public async Task<IActionResult> AddParticipant(/*Todo create new partis view model*/)
-//        {
+            return View(items);
+        }
 
-//            return View();
-//        }
+        /// <summary>
+        /// додає нового учасника в поїздку
+        /// </summary>
+        /// <returns></returns>
 
-//        /// <summary>
-//        /// видаляє учасника з поїздки
-//        /// </summary>
-//        /// <returns></returns>
-//        [HttpGet]
-//        public IActionResult RemoveParticipant(int id, int tourId)
-//        {
-//            return View();
-//        }
+        public IActionResult AddParticipant()
+        {
+            ViewData["Testik"] = new SelectList(_context.Tours, "Id","TourName");
+            ViewData["Testik2"] = new SelectList(_context.Users, "Id", "UserName");
+            return View();
+        }
 
-     
-//    }
-//}
+        [HttpPost]
+        public async Task<IActionResult> AddParticipant(AddParticipantViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.HowFoundUs == null)
+                {
+                    model.HowFoundUs = "null";
+                }
+
+                UserTour userTour = new UserTour{ User = model.User, TourId = model.TourId, HowFoundUs = model.HowFoundUs};
+                _context.Add(userTour);
+                await _context.SaveChangesAsync().ConfigureAwait(true);
+                return RedirectToAction(nameof(GetPartiсipants));
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// видаляє учасника з поїздки
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult RemoveParticipant(int id, int tourId)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ IT_Dnistro/IT_Dnistro/ViewModels/AddParticipantViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IT_Dnistro.ViewModels
+{
+    public class AddParticipantViewModel
+    {
+        public IdentityUser User { get; set; }
+        public int TourId { get; set; }
+        public string HowFoundUs { get; set; }
+    }
+}
