3df87dc1363e3407e377188f366b37813fe92a91
В'ячеслав Дєдов <Diedov270@gmail.com>
2020-03-20 21:26:58 +0200

    1. Змінено галерею, створено один універсальний контролер для додавання фото без привязки до конкретного типу тура 2. Додано можливість вибирати фото по типу тура 3. Виправленно видалення фото

--- IT_Dnistro/IT_Dnistro/Controllers/FileUploadAppController.cs
+++ IT_Dnistro/IT_Dnistro/Controllers/FileUploadAppController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using DataBase;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using IT_Dnistro.ViewModels;
 
 namespace IT_Dnistro.Controllers
 {
@@ -15,87 +17,72 @@ namespace IT_Dnistro.Controllers
     {
         DatabaseContext _context;
         IWebHostEnvironment _appEnvironment;
+        private static int _id;
+        List<DataBase.TourPhoto> photos;
+        List<DataBase.TourType> tours;
 
         public FileUploadAppController(DatabaseContext context, IWebHostEnvironment appEnvironment)
         {
             _context = context;
             _appEnvironment = appEnvironment;
         }
+
         [HttpGet("upload")]
         public IActionResult Index()
         {
-            return View(_context.TourPhotos.ToList());
+            photos = new List<TourPhoto>(_context.TourPhotos);
+            tours = new List<TourType>(_context.TourTypes);
+            // На 1 місце
+            tours.Insert(0, new TourType() { Id = 0, TourTypeName = "Все" });
+            GalleryViewModel gvm = new GalleryViewModel { Tours = tours, Photos = photos };
+            // якщо є id, створює фільтр фільтрація
+            if (_id > 0)
+            {
+                gvm.Photos = photos.Where(p => p.TourTypeId == _id);
+            }
+            return View(gvm);
         }
 
-        [HttpPut("dnistro")]
-        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        [HttpPost]
+        public RedirectToActionResult AddFile(IFormFile uploadedFile)
         {
             if (uploadedFile != null) 
             {
-                string path = "/images/Dnistro/" + uploadedFile.FileName;
+                string path = "/images/Swiper/" + uploadedFile.FileName;
                 using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
                 {
-                    await uploadedFile.CopyToAsync(fileStream).ConfigureAwait(true);
+                     uploadedFile.CopyToAsync(fileStream).ConfigureAwait(true);
                 }
-                TourPhoto file = new TourPhoto() { PhotoLink = uploadedFile.FileName, TourTypeId = 1 };
-                _context.TourPhotos.Add(file);
-                _context.SaveChanges();
+                if(_id != 0)
+                {
+                    TourPhoto file = new TourPhoto() { PhotoLink = uploadedFile.FileName, TourTypeId = _id };
+                    _context.TourPhotos.Add(file);
+                    _context.SaveChanges();
+                }
             }
             return RedirectToAction("Index");
         }
 
-        [HttpDelete]
+        [HttpGet("getId")]
+        public ActionResult GetIdTour(int idTour)
+        {
+            _id = idTour;
+            Console.WriteLine("присвоїти - " + _id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
         public ActionResult DeleteConfirmed(int id)
         {
             var tourPhoto = _context.TourPhotos.Find(id);
             _context.TourPhotos.Remove(tourPhoto);
             _context.SaveChanges();
+            string path = _appEnvironment.WebRootPath + "/images/Swiper/" + tourPhoto.PhotoLink;
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
             return RedirectToAction("Index");
         }
-        [HttpGet("carpaty")]
-        public IActionResult Carpaty()
-        {
-            return View(_context.TourPhotos.ToList());
-        }
-
-        [HttpPut("carpaty")]
-        public async Task<IActionResult> AddFileCarpaty(IFormFile uploadedFile)
-        {
-            if (uploadedFile != null)
-            {
-                string path = "/images/Carpaty/" + uploadedFile.FileName;
-                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
-                {
-                    await uploadedFile.CopyToAsync(fileStream).ConfigureAwait(true);
-                }
-                TourPhoto file = new TourPhoto() { PhotoLink = uploadedFile.FileName, TourTypeId = 2 };
-                _context.TourPhotos.Add(file);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Carpaty");
-        }
-        [HttpGet("scandinavia")]
-        public IActionResult Scandinavia()
-        {
-            return View(_context.TourPhotos.ToList());
-        }
-
-        [HttpPut("scandinavia")]
-        public async Task<IActionResult> AddFileScandinavia(IFormFile uploadedFile)
-        {
-            if (uploadedFile != null)
-            {
-                string path = "/images/Scandinavia/" + uploadedFile.FileName;
-                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
-                {
-                    await uploadedFile.CopyToAsync(fileStream).ConfigureAwait(true);
-                }
-                TourPhoto file = new TourPhoto() { PhotoLink = uploadedFile.FileName, TourTypeId = 3 };
-                _context.TourPhotos.Add(file);
-                _context.SaveChanges();
-            }
-            return RedirectToAction("Scandinavia");
-        }
-
     }
 }
---
+++ IT_Dnistro/IT_Dnistro/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT_Dnistro.ViewModels
+{
+    public class GalleryViewModel
+    {
+        public IEnumerable<DataBase.TourPhoto> Photos { get; set; }
+        public IEnumerable<DataBase.TourType> Tours { get; set; }
+    }
+}
