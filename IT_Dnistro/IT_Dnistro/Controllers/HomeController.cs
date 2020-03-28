26878cb5151cc95f3c937568928e349a3e70f1ca
В'ячеслав Дєдов <Diedov270@gmail.com>
2020-03-28 15:35:18 +0200

    Fix add photo

--- IT_Dnistro/IT_Dnistro/Controllers/HomeController.cs
+++ IT_Dnistro/IT_Dnistro/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using IT_Dnistro.Models;
 using DataBase;
 using System.Linq;
+using IT_Dnistro.ViewModels;
 
 namespace IT_Dnistro.Controllers
 {
@@ -10,15 +12,24 @@ namespace IT_Dnistro.Controllers
     public class HomeController : Controller
     {
         DatabaseContext _db;
+        private static int _idTour = 1;
+        List<TourPhoto> _photos;
+        List<TourType> _tours;
         public HomeController(DatabaseContext context)
         {
             _db = context;
         }
 
+
         [HttpGet("dnistro")]
         public IActionResult Index()
         {
-            return View(_db.TourPhotos.ToList());
+            _photos = new List<TourPhoto>(_db.TourPhotos);
+            _tours = new List<TourType>(_db.TourTypes);
+            GalleryViewModel gvm = new GalleryViewModel { Tours = _tours, Photos = _photos };
+            gvm.Photos = _photos.Where(p => p.TourTypeId == _idTour);
+            gvm.Tours = _tours.Where(p => p.Id == _idTour);
+            return View(gvm);
         }
 
         [HttpGet("carpaty")]
